6cd01e4dcb651a742069a6b2335a82c6823078f4
Thiago Butignon <thiagobutignon@gmail.com>
2023-11-10 17:01:28 -0300

    refactor: move logic business logic to a hook

---
+++ src/presentation/hooks/use-bank-account.ts
@@ -0,0 +1,76 @@
+import { useEffect, useState } from 'react';
+
+import { GetBankAccount } from '../../domain/models';
+
+const useBankAccount = (getBankAccount: GetBankAccount): any => {
+  const [state, setState] = useState({
+    accounts: [] as GetBankAccount.Response,
+    isLoading: false,
+    error: ''
+  });
+  const [selectedAccountIndex, setSelectedAccountIndex] = useState<number | null>(null);
+  const [showBarChart, setShowBarChart] = useState<boolean>(false);
+  const [barChartData, setBarChartData] = useState<Array<{ x: string, y: number }>>([]);
+  const [transactionDescriptions, setTransactionDescriptions] = useState<string[]>([]);
+
+  useEffect(() => {
+    setState(old => ({ ...old, isLoading: true }));
+    getBankAccount.perform()
+      .then(accounts => {
+        setState(old => ({ ...old, accounts, isLoading: false }));
+      })
+      .catch(error => {
+        console.log(error);
+        setState(old => ({ ...old, accounts: [], isLoading: false }));
+      });
+  }, [getBankAccount]);
+
+  const handleDataPointSelection = (event: any, chartContext: any, config: any): void => {
+    setSelectedAccountIndex(config.dataPointIndex);
+    const accountType = chartLabels[config.dataPointIndex];
+    const selectedAccount = state.accounts.find(account => account.accountType === accountType);
+
+    if (selectedAccount?.transactions) {
+      const transactionData = selectedAccount.transactions.reduce<Record<string, number>>((acc, transaction) => {
+        acc[transaction.date] = (acc[transaction.date] || 0) + transaction.amount;
+        return acc;
+      }, {});
+      const descriptions = selectedAccount.transactions.map(transaction => transaction.description);
+      setTransactionDescriptions(descriptions);
+      setBarChartData(Object.entries(transactionData).map(([date, amount]) => ({ x: date, y: amount })));
+      setShowBarChart(true);
+    }
+  };
+
+  const makeAccountTypes = (): string[] => state.accounts.map(account => account.accountType);
+
+  const aggregateBalances = (): number[] => {
+    const balanceMap: Record<string, number> = {};
+    state.accounts.forEach(account => {
+      const { accountType, balance } = account;
+      balanceMap[accountType] = (balanceMap[accountType] || 0) + balance.current;
+    });
+    return Object.values(balanceMap);
+  };
+
+  const aggregateDates = (): string[] | undefined => {
+    return selectedAccountIndex != null ? state.accounts[selectedAccountIndex].transactions?.map(transaction => transaction.date) : []
+  }
+
+  const chartLabels = makeAccountTypes()
+
+  return {
+    state,
+    handleDataPointSelection,
+    makeAccountTypes,
+    aggregateBalances,
+    aggregateDates,
+    showBarChart,
+    setShowBarChart,
+    barChartData,
+    setBarChartData,
+    transactionDescriptions
+  };
+};
+
+export default useBankAccount;
--- src/presentation/pages/home.tsx
+++ src/presentation/pages/home.tsx
@@ -1,57 +1,27 @@
 import './home.css'
 
 import { AccountList, ChartBox } from '../components'
-import React, { useEffect, useState } from 'react'
 
 import { ApexOptions } from 'apexcharts'
 import { GetBankAccount } from '../../domain/models'
+import React from 'react'
+import useBankAccount from '../hooks/use-bank-account'
 
 type Props = {
   getBankAccount: GetBankAccount
 }
 
 export const Home: React.FC<Props> = ({ getBankAccount }) => {
-  const [state, setState] = useState({
-    accounts: [] as GetBankAccount.Response,
-    isLoading: false,
-    error: ''
-  })
-  const [selectedAccountIndex, setSelectedAccountIndex] = useState<number | null>(null);
-  const [showBarChart, setShowBarChart] = useState<boolean>(false)
-  const [barChartData, setBarChartData] = useState<Array<{ x: string, y: number }>>([]);
-  const [transactionDescriptions, setTransactionDescriptions] = useState<string[]>([]);
-
-  const handleDataPointSelection = (event: any, chartContext: any, config: any): void => {
-    setSelectedAccountIndex(config.dataPointIndex);
-    const accountType = chartLabels[config.dataPointIndex];
-    const selectedAccount = state.accounts.find(account => account.accountType === accountType);
-
-    if (selectedAccount?.transactions) {
-      const transactionData = selectedAccount.transactions.reduce<Record<string, number>>((acc, transaction) => {
-        acc[transaction.date] = (acc[transaction.date] || 0) + transaction.amount;
-        return acc;
-      }, {});
-      const descriptions = selectedAccount.transactions.map(transaction => transaction.description);
-      setTransactionDescriptions(descriptions);
-      setBarChartData(Object.entries(transactionData).map(([date, amount]) => ({ x: date, y: amount })));
-      setShowBarChart(true);
-    }
-  };
-
-  const makeAccountTypes = (): string[] => state.accounts.map(account => account.accountType);
-
-  const aggregateBalances = (): number[] => {
-    const balanceMap: Record<string, number> = {};
-    state.accounts.forEach(account => {
-      const { accountType, balance } = account;
-      balanceMap[accountType] = (balanceMap[accountType] || 0) + balance.current;
-    });
-    return Object.values(balanceMap);
-  };
-
-  const aggregateDates = (): string[] | undefined => {
-    return selectedAccountIndex != null ? state.accounts[selectedAccountIndex].transactions?.map(transaction => transaction.date) : []
-  }
+  const {
+    state,
+    handleDataPointSelection,
+    makeAccountTypes,
+    aggregateBalances,
+    aggregateDates,
+    showBarChart,
+    barChartData,
+    transactionDescriptions
+  } = useBankAccount(getBankAccount);
 
   const pieOptions: ApexOptions = {
     chart: {
@@ -98,20 +68,7 @@ export const Home: React.FC<Props> = ({ getBankAccount }) => {
     }
   }
 
-  const chartLabels = makeAccountTypes()
   const chartSeries = aggregateBalances()
-
-  useEffect(() => {
-    setState(old => ({ ...old, isLoading: true }))
-    getBankAccount.perform()
-      .then(accounts => {
-        setState(old => ({ ...old, accounts, isLoading: false }));
-      })
-      .catch(error => {
-        console.log(error)
-        setState(old => ({ ...old, accounts: [], isLoading: false }))
-      })
-  }, [getBankAccount])
 
   return (
     <>

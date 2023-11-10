8f6acd50dc930dcd8e0afe9cfdf1444ac7ba535d
Thiago Butignon <thiagobutignon@gmail.com>
2023-11-10 16:35:10 -0300

    test: add coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/index.js',
+    '!src/reportWebVitals.js',
+    '!src/setupTests.js'
+  ],
   transformIgnorePatterns: ['/node_modules/(?!axios).+\\.js$'],
   preset: 'ts-jest',
   testEnvironment: 'node',
